7d4a6be758f4afb205d77d4fcf26494282a18dd2
Rishi <l.rishissahu@gmail.com>
2023-04-19 23:36:13 +0530

    date-19

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -1,8 +1,14 @@
 import addGlobalEventListner from "./utils/addGlobalEventListner.js";
 
-
 export default function setup() {
-    addGlobalEventListner('mousedown', '[data-draggable]', _e => {
-        console.log('mosue down')
+    addGlobalEventListner('mousedown', '[data-draggable]', e => {
+        console.log('mouse down')
+        const moveMouseFunc = () => {
+            console.log('moving mouse')
+        }
+        document.addEventListener('mousemove', moveMouseFunc)
+        document.addEventListener('mouseup', () => {
+            document.removeEventListener('mousemove', moveMouseFunc)
+        })
     })
 }
