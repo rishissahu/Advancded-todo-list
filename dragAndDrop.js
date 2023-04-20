337fa284a386f3c495eee140b4243741e53d7004
Rishi <l.rishissahu@gmail.com>
2023-04-21 01:11:00 +0530

    dragging elements implemented

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -4,17 +4,30 @@ export default function setup() {
     addGlobalEventListner('mousedown', '[data-draggable]', e => {
         const selectedItem = e.target;
         const cloneSelected = selectedItem.cloneNode(true)
+        positionColne(cloneSelected, e)
+        document.body.appendChild(cloneSelected)
+        cloneSelected.classList.add('dragging')
         selectedItem.classList.add('hide')
 
-        // console.log(selected)
+        console.log(selectedItem.innerText)
         console.log('mouse down')
-        const moveMouseFunc = () => {
+        const moveMouseFunc = e => {
             console.log('moving mouse')
+            positionColne(cloneSelected, e)
+            document.body.appendChild(cloneSelected)
+
         }
         document.addEventListener('mousemove', moveMouseFunc)
         document.addEventListener('mouseup', () => {
             document.removeEventListener('mousemove', moveMouseFunc)
             selectedItem.classList.remove('hide')
+            cloneSelected.remove()
+
         })
     })
+}
+
+function positionColne(itemClone, mouseEvent) {
+    itemClone.style.top = `${mouseEvent.clientY}px`
+    itemClone.style.left = `${mouseEvent.clientX}px`
 }
